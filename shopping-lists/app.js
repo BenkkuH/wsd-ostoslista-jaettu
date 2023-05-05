cfcec861ce0d7972f4dfdc4a56c11f25fb1e7ea7
benkku <benkkuheist@gmail.com>
2023-05-05 13:34:40 +0300

    testi2

--- shopping-lists/app.js
+++ shopping-lists/app.js
@@ -2,7 +2,7 @@ import { serve } from "./deps.js";
 
 const handleRequest = async (request) => {
   console.log("Responding with Hello world!");
-  return new Response("Hello!");
+  return new Response("Heippa maailma");
 };
 
 serve(handleRequest, { port: 7777 });
