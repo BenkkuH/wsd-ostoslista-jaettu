cac62496cd3408957b4283e3729323fb191d1b89
benkku <benkkuheist@gmail.com>
2023-06-06 17:25:36 +0300

    Etusivun tabititletesti, listan luontitesti, listan avaustesti toimii

--- e2e-playwright/tests/shopping_lists_tests.spec.js
+++ e2e-playwright/tests/shopping_lists_tests.spec.js
@@ -1,5 +1,29 @@
 const { test, expect } = require("@playwright/test");
 
+test("Main page has expected title.", async ({ page }) => {
+  await page.goto("/");
+  await expect(page).toHaveTitle("Shared shopping lists");
+});
+
+test("Can create a list.", async ({ page }) => {
+  await page.goto("/lists");
+  const listName = `My list: ${Math.random()}`;
+  await page.locator("input[type=text]").type(listName);
+  await page.locator("input[type=submit]").first().click();
+  await expect(page.locator(`a >> text='${listName}'`)).toHaveText(listName);
+});
+
+test("Can open a list.", async ({ page }) => {
+  await page.goto("/lists");
+  const listName = `My list: ${Math.random()}`;
+  await page.locator("input[type=text]").type(listName);
+  await page.locator("input[type=submit]").first().click();
+  await page.locator(`a >> text='${listName}'`).click();
+  await expect(page.locator("h1")).toHaveText(listName);
+});
+
+
+/*
 test("Main page has expected title and headings.", async ({ page }) => {
   await page.goto("/");
   await expect(page).toHaveTitle("Task application!");
@@ -9,24 +33,25 @@ test("Main page has expected title and headings.", async ({ page }) => {
 
 test("Can create a task.", async ({ page }) => {
   await page.goto("/");
-  const taskName = `My task: ${Math.random()}`;
-  await page.locator("input[type=text]").type(taskName);
+  const listName = `My task: ${Math.random()}`;
+  await page.locator("input[type=text]").type(listName);
   await page.locator("input[type=submit]").click();
-  await expect(page.locator(`a >> text='${taskName}'`)).toHaveText(taskName);
+  await expect(page.locator(`a >> text='${listName}'`)).toHaveText(listName);
 });
 
 test("Can open a task page.", async ({ page }) => {
   await page.goto("/");
-  const taskName = `My task: ${Math.random()}`;
-  await page.locator("input[type=text]").type(taskName);
+  const listName = `My task: ${Math.random()}`;
+  await page.locator("input[type=text]").type(listName);
   await page.locator("input[type=submit]").click();
-  await page.locator(`a >> text='${taskName}'`).click();
-  await expect(page.locator("h1")).toHaveText(taskName);
+  await page.locator(`a >> text='${listName}'`).click();
+  await expect(page.locator("h1")).toHaveText(listName);
 });
-
+*/
 //Viisi testiä - yllä olevien mukaan/lisäksi:
 
 /*  - Voi lisätä listoja
     - Voi deaktivoida listoja
     - Voi lisätä tuotteita listalle
-    - Voi merkitä tuotteita kerätyksi */
+    - Voi merkitä tuotteita kerätyksi
+    - list.eta näyttää oikean ostoslistan otsikon */
