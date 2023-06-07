528d34af53fb12df3fb5e2c54fcaf69141c6740a
JohannnaM <git@github.com:JohannnaM/Osa14_4.git>
2023-06-07 10:46:30 +0300

    can add an item -testin kokeilua

--- e2e-playwright/tests/shopping_lists_tests.spec.js
+++ e2e-playwright/tests/shopping_lists_tests.spec.js
@@ -22,35 +22,22 @@ test("Can open a list.", async ({ page }) => {
   await expect(page.locator("h1")).toHaveText(listName);
 });
 
-
-/*
-test("Main page has expected title and headings.", async ({ page }) => {
-  await page.goto("/");
-  await expect(page).toHaveTitle("Task application!");
-  await expect(page.locator("h1")).toHaveText("Tasks");
-  await expect(page.locator("h2")).toHaveText(["Add a task", "Active tasks"]);
-});
-
-test("Can create a task.", async ({ page }) => {
-  await page.goto("/");
-  const listName = `My task: ${Math.random()}`;
+test("Can add an item to a list", async ({ page }) => {
+  await page.goto("/lists");
+  const listName = `My list: ${Math.random()}`;
   await page.locator("input[type=text]").type(listName);
-  await page.locator("input[type=submit]").click();
-  await expect(page.locator(`a >> text='${listName}'`)).toHaveText(listName);
-});
-
-test("Can open a task page.", async ({ page }) => {
-  await page.goto("/");
-  const listName = `My task: ${Math.random()}`;
-  await page.locator("input[type=text]").type(listName);
-  await page.locator("input[type=submit]").click();
+  await page.locator("input[type=submit]").first().click();
   await page.locator(`a >> text='${listName}'`).click();
   await expect(page.locator("h1")).toHaveText(listName);
+  const itemName = `My item: ${Math.random()}`;
+  await page.locator("input[type=text]").type(itemName);
+  await page.locator("input[type=submit]").first().click();
+  await expect(page.locator(`li >> text='${itemName}'`)).first().toHaveText(itemName);
 });
-*/
+
 //Viisi testiä - yllä olevien mukaan/lisäksi:
 
-/*  - Voi lisätä listoja
+/*  
     - Voi deaktivoida listoja
     - Voi lisätä tuotteita listalle
     - Voi merkitä tuotteita kerätyksi
